7fbf67e69db8f8bd6ba71f5054c4556a32063daa
emilio barrera <barrera_emilio@yahoo.es>
2023-02-16 14:25:23 +0000

    all done

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -3,10 +3,10 @@ using Sales.Shared.Entities;
 
 namespace Sales.API.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options): base(options)
-        {}
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
 
         public DbSet<Country> Countries { get; set; }
 
